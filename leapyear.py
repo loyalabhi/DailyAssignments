8f0ac78b77059b654013e5b48018e422da4b4a71
loyalabhi <abhighanan12@gmail.com>
2023-04-19 10:18:51 +0530

    first commit

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+#3. Write a program to print if the given number is zero or odd or even.
+
+num = int (input (“Enter any number to test whether it is odd or even: “)
+
+if(num==0):
+     print("The number is zero")
+elif(num % 2) == 0:
+     print (“The number is even”)
+
+else:
+     print (“The provided number is odd”)
